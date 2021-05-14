577b9aa3fd8244fc2f65767331ad4c69e232bebf
Junliang Yan <jyan@ca.ibm.ca>
2021-05-14 15:06:42 -0400

    update testdriver and testgen

--- testdriver.c
+++ testdriver.c
@@ -21,6 +21,11 @@ bool one_of_strings(char* target, char* strings[], int length) {
 
 size_t getline(char**, size_t*, FILE*);
 
+int qr_length(query_result_t qr) {
+  if (qr == NULL) return 0;
+  return qr_length(qr->next) + 1;
+}
+
 int main() {
 
   char* buf = (char*)malloc(1024);
@@ -33,48 +38,57 @@ int main() {
     if (strcmp(opcode, "create_db") == 0) {
       db = create_db(operand);
       if (db != NULL) {
-        printf("Success.\n");
+        printf("create_db Success.\n");
       } else {
-        printf("Fail.\n");
+        printf("create_db Fail.\n");
       }
     } else if (strcmp(opcode, "open_db") == 0) {
       db = open_db(operand);
       if (db != NULL) {
-        printf("Success.\n");
+        printf("open_db Success.\n");
       } else {
-        printf("Fail.\n");
+        printf("open_db Fail.\n");
       }
     } else if (strcmp(opcode, "close_db") == 0) {
       close_db(db);
-      printf("Success.\n");
+      printf("close_db Success.\n");
     } else if (strcmp(opcode, "get") == 0) {
       key_t key = operand;
       val_t val = get(db, key);
-      printf("RET = %s\n", val == NULL ? "NULL": val);
+      printf("get RET = %s\n", val == NULL ? "NULL": val);
     } else if (strcmp(opcode, "put") == 0) {
       key_t key = operand;
       val_t val = operand2;
       bool ret = put(db, key, val);
-      printf("RET = %s\n", ret ? "true" : "false");
+      printf("put RET = %s\n", ret ? "true" : "false");
     } else if (strcmp(opcode, "remove_key") == 0) {
       key_t key = operand;
       bool ret = remove_key(db, key);
-      printf("RET = %s\n", ret ? "true" : "false");
+      printf("remove_key RET = %s\n", ret ? "true" : "false");
     } else if (strcmp(opcode, "update") == 0) {
       key_t key = operand;
       val_t val = operand2;
       bool ret = update(db, key, val);
-      printf("RET = %s\n", ret ? "true" : "false");
+      printf("update RET = %s\n", ret ? "true" : "false");
     } else if (strcmp(opcode, "query") == 0) {
       val_t val = operand;
       query_result_t qr = query(db, val);
-      printf("RET = %s", qr == NULL ? "NULL" : qr->key);
-      query_result_t cur = qr;
-      while(cur != NULL) {
-        cur = cur->next;
-        printf(",%s", cur->key);
+      int length = qr_length(qr);
+      if (length == 0) {
+        printf("query RET = NULL\n");
+      } else {
+        char* result[length];
+        query_result_t cur = qr;
+        for (int i = 0; i < length; i++, cur = cur->next) {
+          result[i] = cur->key;
+        }
+        qsort(result, length, sizeof(char*), strcmp);
+        printf("query RET = %s", result[0]);
+        for (int i = 1; i < length; i++) {
+          printf(",%s", result[i]);
+        }
+        printf("\n");
       }
-      printf("\n");
       delete_query_result(qr);
     }
   }
--- testgen.py
+++ testgen.py
@@ -1,16 +1,34 @@
+#!/usr/bin/env python3
 import random
-
-ops = ['get', 'put', 'update', 'remove_key']
-keys = [ 'key' + str(random.randint(0, 100)) for i in range(30) ]
-values = [ 'val' + str(random.randint(0, 100)) for i in range(30) ]
+import argparse
+import sys
 
 def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--seed', "-s", default=0, help="random see.")
+    parser.add_argument('--input', "-i", default=None, help="input file.")
+    parser.add_argument('--expected', "-e", default=None, help="expected file.")
+
+    args = parser.parse_args()
+    seed = int(args.seed)
+    random.seed(seed)
+
+    inp = sys.stdout
+    if args.input != None:
+        inp = open(args.input, 'w')
+
+    expected_file_name = 'expected.txt' if args.expected == None else args.expected
+    out = open(expected_file_name, 'w')
+
     db = {}
-    inp = open("input.txt", 'w')
-    out = open('expected.txt', 'w')
+    ops = ['get', 'put', 'update', 'remove_key', 'query']
+    keys = [ 'key' + str(random.randint(0, 100)) for i in range(30) ]
+    values = [ 'val' + str(random.randint(0, 100)) for i in range(30) ]
+
 
     inp.write("create_db db1\n")
-    out.write('Success.\n')
+    out.write('create_db Success.\n')
 
     for i in range(100):
         op = random.choice(ops)
@@ -18,7 +36,7 @@ def main():
             key = random.choice(keys)
             inp.write("get %s\n" % key)
             if key not in db.keys():
-                out.write('RET = NULL\n')
+                out.write('get RET = NULL\n')
             else:
                 out.write('RET = %s\n' % db[key])
         elif op == 'put':
@@ -27,26 +45,40 @@ def main():
             inp.write("put %s %s\n" % (key, val))
             if key not in db.keys():
                 db[key] = val
-                out.write('RET = true\n')
+                out.write('put RET = true\n')
             else:
-                out.write('RET = false\n')
+                out.write('put RET = false\n')
         elif op == 'update':
             key = random.choice(keys)
             val = random.choice(values)
             inp.write("update %s %s\n" % (key, val))
             if key not in db.keys():
-                out.write('RET = false\n')
+                out.write('update RET = false\n')
             else:
                 db[key] = val
-                out.write('RET = true\n')
+                out.write('update RET = true\n')
         elif op == 'remove_key':
             key = random.choice(keys)
-            inp.write("update %s %s\n" % (key, val))
+            inp.write("remove_key %s\n" % key)
             if key not in db.keys():
-                out.write('RET = false\n')
+                out.write('remove_key RET = false\n')
             else:
                 del db[key]
-                out.write('RET = true\n')
+                out.write('remove_key RET = true\n')
+        elif op == 'query':
+            val = random.choice(values)
+            inp.write("query %s\n" % val)
+            if val not in db.values():
+                out.write("query RET = NULL\n")
+            else:
+                res = []
+                for k in db.keys():
+                    if db[k] == val:
+                        res.append(k)
+                res.sort()
+                res = ','.join(res)
+                out.write("query RET = %s\n" % res)
+
     inp.close()
     out.close()
 
