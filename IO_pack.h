1eef9aef91f1206dfbc2ffc5d93a47c4e9ec57c7
C.C <charlie.chen0228@gmail.com>
2021-05-24 20:45:33 -0400

    formated IO_pack.h IO_pack.c

--- IO_pack.h
+++ IO_pack.h
@@ -4,18 +4,17 @@
 #include<stdio.h>
 #include<errno.h>
 
-typedef struct 
-{
-  int io_fd;
-  int io_cnt;
-  char* io_bufp;
-  char io_buf[8192];
+typedef struct {
+	int io_fd;
+	int io_cnt;
+	char *io_bufp;
+	char io_buf[8192];
 } io_t;
 
-ssize_t io_readn(int fd, void* userbuf, size_t n);
+ssize_t io_readn(int fd, void *userbuf, size_t n);
 
-ssize_t io_writen(int *rp, void* userbuf, size_t n);
+ssize_t io_writen(int *rp, void *userbuf, size_t n);
 
-ssize_t io_wrten_buff(io_t* rp , void* userbuf, size_t n);
+ssize_t io_wrten_buff(io_t * rp, void *userbuf, size_t n);
 
-ssize_t io_readline_buff(io_t *rp, void* userbuf, size_t maxlen);
+ssize_t io_readline_buff(io_t * rp, void *userbuf, size_t maxlen);
